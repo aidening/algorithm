376f822700ce4f4e275ad8b0e0e5268120bdb68f
ling.xiong <ling.xiong@hand-china.com>
2020-04-02 17:37:16 +0800

    [ADD] 新增冒泡排序算法&文档说明

--- src/排序算法/冒泡排序.js
+++ src/排序算法/冒泡排序.js
@@ -1,18 +1,21 @@
 /**
  * 时间复杂度O(n*n)
+ * 原理：分两层循环。
+ *      1、外层循环 n 次；
+ *      2、内层循环 n - i 次，相邻元素比较，前者大于后者则互换位置
  */
-function bubbleSort(treeArray) {
+function bubbleSort(testDataArr) {
   // 两层for循环
-  for (let i = 1; i < treeArray.length; i++) {
+  for (let i = 1; i < testDataArr.length; i++) {
     // 内层比较
-    for (let j = 0; j < treeArray.length - i; j++) {
-      if (treeArray[j] > treeArray[j + 1]) {
-        [treeArray[j], treeArray[j + 1]] = [treeArray[j + 1], treeArray[j]];
+    for (let j = 0; j < testDataArr.length - i; j++) {
+      if (testDataArr[j] > testDataArr[j + 1]) {
+        [testDataArr[j], testDataArr[j + 1]] = [testDataArr[j + 1], testDataArr[j]];
       }
     }
   }
-  return treeArray;
+  return testDataArr;
 }
 
-const treeArray = [5, 6, 8, 1, 233, 0, 9, 3, 78, 24, 99, 333, 111, 5, 666];
-console.log(bubbleSort(treeArray), treeArray);
+const testDataArr = [5, 6, 8, 1, 233, 0, 9, 3, 78, 24, 99, 333, 111, 5, 666];
+console.log(bubbleSort(testDataArr), testDataArr);
